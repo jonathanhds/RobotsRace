d2b8c7405081ceb68363fc3b8e0c312807ec7747
Jonathan Henrique de Souza <jonathan.henrique@icloud.com>
2023-08-11 20:59:20 -0300

    First commit

---
+++ RobotsRace/RobotsRaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobotsRaceApp.swift
+//  RobotsRace
+//
+//  Created by Jonathan Souza on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct RobotsRaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
